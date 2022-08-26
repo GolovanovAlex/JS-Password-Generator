8279a7d8cabc3caf4796bbc4980d852ab94f5537
GolovanovAlex <Golovanov.a.a@yandex.ru>
2022-08-26 14:55:02 +0300

    js

---
+++ app.js
@@ -0,0 +1,4 @@
+const display = document.querySelector('input');
+const button = document.querySelector('button');
+const copyBtn = document.querySelector('span.far');
+const copyActive = document.querySelector('span.fas');
