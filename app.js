a81304b98fa2c7922a4d55679035ef50dd49d636
GolovanovAlex <Golovanov.a.a@yandex.ru>
2022-08-26 15:05:12 +0300

    js fin

--- app.js
+++ app.js
@@ -2,3 +2,23 @@ const display = document.querySelector('input');
 const button = document.querySelector('button');
 const copyBtn = document.querySelector('span.far');
 const copyActive = document.querySelector('span.fas');
+const chars =
+  'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+~`|}{[]:;?><,./-=';
+
+button.addEventListener('click', () => {
+  let randomPassword = '';
+  copyBtn.style.display = 'block';
+  copyActive.style.display = 'none';
+
+  for (var i = 0; i < 16; i++) {
+    randomPassword += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  display.value = randomPassword;
+});
+
+const copy = () => {
+  copyBtn.style.display = 'none';
+  copyActive.style.display = 'block';
+  display.select();
+  document.execCommand('copy');
+};
